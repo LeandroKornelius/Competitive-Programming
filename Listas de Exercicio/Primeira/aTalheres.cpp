97e549934255bb1255ecc9f3b138ca602f5cc95e
Leandro Kornelius <leobelko@gmail.com>
2023-09-25 23:10:19 -0300

    feat:lista de exs

---
+++ Listas de Exercicio/Primeira/aTalheres.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int pessoas, garfos, facas, colheres;
+    cin >> pessoas;
+    cin >> garfos;
+    cin >> facas;
+    cin >> colheres;
+
+    if (garfos > facas) {
+        if (pessoas < facas + colheres) {
+            cout << pessoas << "\n";
+        } else {
+            cout << facas + colheres << "\n";
+        }
+    } else { 
+        if (pessoas < garfos + colheres) {
+            cout << pessoas << "\n";
+        } else {
+            cout << garfos + colheres << "\n";
+        }
+    } 
+
+
+    return 0;
+}
