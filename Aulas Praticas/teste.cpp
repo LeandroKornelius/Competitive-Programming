bbe41fb5d37b132050e97d22569e73c148dc8a60
Leandro Kornelius <leobelko@gmail.com>
2023-10-31 14:30:15 -0300

    feat: lista pratica do linf

--- Aulas Praticas/teste.cpp
+++ Aulas Praticas/teste.cpp
@@ -1,22 +1,11 @@
 #include<bits/stdc++.h>
-#include <vector> 
 using namespace std;
 
-int dp[10000];
-
-long long int fib(int n)
-{
-    if(dp[n] != 0) return dp[n];
-    if (n <= 1) return n;
-    return  fib(n - 1) + fib(n - 2);
-}
-
 int main() {
+    std::ios::sync_with_stdio(false);
+    cin.tie(NULL);
 
-    for(int i = 0; i < 5000; ++i) {
-        dp[i] = 0;
-    }
-    cout << fib(40) << "\n";
+    
 
     return 0;
 }
