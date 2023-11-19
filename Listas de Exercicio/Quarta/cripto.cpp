246dd802b3eac48bbf1e2a1c698641f6ae318536
Leandro Kornelius <leobelko@gmail.com>
2023-11-19 12:29:22 -0300

    fix: arrumei ult quest

--- Listas de Exercicio/Quarta/cripto.cpp
+++ Listas de Exercicio/Quarta/cripto.cpp
@@ -8,9 +8,34 @@ int main() {
     std::ios::sync_with_stdio(false);
     cin.tie(NULL);
     
-    int n, m, c;
+    int n, m, c, aux;
     cin >> n >> m >> c;
     
-    
+    vector<int> a;
+    vector<int> b;
+
+    for (int i = 0; i < n; i++) {
+        cin >> aux;
+        a.push_back(aux);
+    }
+
+    for (int i = 0; i < m; i++) {
+        cin >> aux;
+        b.push_back(aux);
+    }
+
+    ll etapas = 0;
+    while (etapas < (n - m + 1)) {
+        for(int i = 0; i < m; i++) {
+            a[etapas + i] = (a[etapas + i] + b[i]) % c;
+        }
+        etapas++;
+    }
+
+    for (int i = 0; i < n; i++) {
+        cout << a[i] << " ";
+    }
+
     return 0;
 }
+
