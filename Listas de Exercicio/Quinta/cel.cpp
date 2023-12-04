b4c7639490f8d63b673e473656b564e43c6edb5c
Leandro Kornelius <leobelko@gmail.com>
2023-12-03 21:26:26 -0300

    feat: tentativa de sol pra ult quest

--- Listas de Exercicio/Quinta/cel.cpp
+++ Listas de Exercicio/Quinta/cel.cpp
@@ -1,33 +1,10 @@
-#include<bits/stdc++.h>
+#include <iostream>
+#include <vector>
+
 using namespace std;
 
-#define ll long long
-
-// ACCEPTED
 int main() {
-    std::ios::sync_with_stdio(false);
-    cin.tie(NULL);
     
-    int t, n;
-    vector<int> nums;
-    cin >> t;
-
-    for (int i = 0; i < t; i++) {
-        cin >> n;
-        int maiorPot = 1;
-        while (maiorPot < n) {
-            maiorPot = maiorPot << 1;
-        }
-        maiorPot = maiorPot >> 1;
-        cout << maiorPot << " ";
-        for (int j = 0; j < n; j++) {
-            if (j != maiorPot) {
-                cout << j << " ";
-            }
-        }
-        cout << endl;       
-    }
-
 
     return 0;
 }
