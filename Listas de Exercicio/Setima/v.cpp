d4864215c0cddfbb54755f8fb31321f3bd10f61d
Leandro Kornelius <leobelko@gmail.com>
2023-12-20 00:59:03 -0300

    fix: proper bin search using lower

--- Listas de Exercicio/Setima/v.cpp
+++ Listas de Exercicio/Setima/v.cpp
@@ -3,63 +3,16 @@ using namespace std;
 
 #define ll long long
 
-int buscaBinL(vector<int> v, int query) {
-    
-    int l = 0;
-    int r = v.size() - 1;
-    int result = -1;
-
-    while (l <= r) {
-        int mid = (l + r) / 2;
-
-        if (v[mid] == query) {
-            result = mid;
-            r = mid - 1; // mais a esq
-        } else if (v[mid] < query) {
-            l = mid + 1;
-        } else {
-            r = mid - 1;
-        }
-    }
-
-    return result;
-}
-
-int buscaBinR(vector<int> v, int query) {
-    
-    int l = 0;
-    int r = v.size() - 1;
-    int result = -1;
-
-    while (l <= r) {
-        int mid = (l + r) / 2;
-
-        if (v[mid] == query) {
-            result = mid;
-            l = mid + 1; // mais a dir
-        } else if (v[mid] < query) {
-            l = mid + 1;
-        } else {
-            r = mid - 1;
-        }
-    }
-
-    return result;
-}
-
-// ACCEPTED
 int main() {
     cin.tie(NULL);
 
     int n, m;
-    cin >> n >> m; /// livros na livraria e qnt money
+    cin >> n >> m; // livros na livraria e quantia de dinheiro
 
-    vector<int> p;
+    vector<int> p(n);
 
     for (int i = 0; i < n; i++) {
-        int aux;
-        cin >> aux;
-        p.push_back(aux);
+        cin >> p[i];
     }
 
     sort(p.begin(), p.end());
@@ -68,26 +21,13 @@ int main() {
     pair<int, int> menores;
 
     for (int i = 0; i < n; i++) {
-        int indProcurado = buscaBinL(p, m - p[i]);
-        bool indDif = true;
-        if (i == indProcurado) {
-            // eh o mesmo indice, mas pode ter igual em outra pos
-            indDif = false;
-            indProcurado = buscaBinR(p, m - p[i]);
-            if (i != indProcurado) {
-                indDif = true;
-            }
-        }
-        if (indDif) {
-            if (menorDif > abs(p[i] - p[indProcurado])) {
-                if (p[i] <= p[indProcurado]) {
-                            menores.first = p[i];
-                            menores.second = p[indProcurado];
-                        } else {
-                            menores.first = p[indProcurado];
-                            menores.second = p[i];
-                        }
-                        menorDif = abs(p[i] - p[indProcurado]);
+        int indComp = lower_bound(p.begin(), p.end(), m - p[i]) - p.begin();
+
+        if (indComp < n && indComp != i && p[indComp] == m - p[i]) {
+            if (menorDif > abs(p[i] - p[indComp])) {
+                menores.first = min(p[i], p[indComp]);
+                menores.second = max(p[i], p[indComp]);
+                menorDif = abs(p[i] - p[indComp]);
             }
         }
     }
@@ -96,3 +36,5 @@ int main() {
 
     return 0;
 }
+
+
