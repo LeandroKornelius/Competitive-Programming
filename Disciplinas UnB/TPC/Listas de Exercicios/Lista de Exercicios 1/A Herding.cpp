d7faa83942d562a3f6f8154f93054f44a657ef9a
Leandro Kornelius <leobelko@gmail.com>
2024-11-20 01:30:25 -0300

    :sparkles: feat: DFS solution that reaches TLE

--- Disciplinas UnB/TPC/Listas de Exercicios/Lista de Exercicios 1/A Herding.cpp
+++ Disciplinas UnB/TPC/Listas de Exercicios/Lista de Exercicios 1/A Herding.cpp
@@ -1,20 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void dfs(int startingLine, int startingColumn, vector<vector<char>>& graph, vector<vector<bool>>& visited) {
-    visited[startingLine][startingColumn] = true;
+int dfs(int i, int j, int id, vector<vector<char>>& city, vector<vector<pair<bool, int>>>& tracker) {
+    int nextI = i, nextJ = j;
 
-    // Sees what is next square it need to go to
-    int nextLine = startingLine, nextColumn = startingColumn;
-    if (graph[startingLine][startingColumn] == 'N') nextLine -= 1;
-    else if (graph[startingLine][startingColumn] == 'S') nextLine += 1;
-    else if (graph[startingLine][startingColumn] == 'E') nextColumn += 1;
-    else if (graph[startingLine][startingColumn] == 'W') nextColumn -= 1;
+    // Based on the direction of the current square, will adapt the index of the next square
+    if (city[i][j] == 'N') nextI -= 1;
+    else if (city[i][j] == 'S') nextI += 1;
+    else if (city[i][j] == 'E') nextJ += 1;
+    else if (city[i][j] == 'W') nextJ -= 1;
 
-    // Checks to see if next has been visited, if not it needs to go to it
-    if (!visited[nextLine][nextColumn]) {
-        dfs(nextLine, nextColumn, graph, visited);
+    // Updates the current square to visited
+    tracker[i][j].first = true;
+
+    if (tracker[nextI][nextJ].first) {
+        // It has already visited the next square
+        tracker[i][j].second = tracker[nextI][nextJ].second;
+    } else {
+        // It has not visited the next square
+        tracker[i][j].second = dfs(nextI, nextJ, id, city, tracker);
     }
+    return tracker[i][j].second;
+}
+
+// Complexity - O(n)
+bool inside(vector<int> v, int n) {
+    for (int i = 0; i < v.size(); i++) {
+        if (v[i] == n) {
+            return true;
+        }
+    }
+    return false;
 }
 
 int main() {
@@ -24,28 +40,41 @@ int main() {
     int n, m;
     cin >> n >> m;
 
-    // Matriz will hold the directions in type char of each square in the city
+    // Matrix will hold the direction of each square in the city
     vector<vector<char>> city(n, vector<char>(m));
-    // Matrix will hold if the square has been visited or not
-    vector<vector<bool>> visited(n, vector<bool>(m, false));
+    // Matrix will have if the square has been visited and the graph id it will eventually lead to 
+    vector<vector<pair<bool, int>>> tracker(n, vector<pair<bool, int>>(m));
 
-    // Forms the matriz with its directions
+
+    int identifier = 1;
+    // Forms the matrix with its directions and tracking information
     for (int i = 0; i < n; i++) {
         string aux;
         cin >> aux;
         for (int j = 0; j < m; j++) {
             city[i][j] = aux[j];
+            tracker[i][j].first = false;
+            tracker[i][j].second = identifier;
+            identifier++;
         }
     }
-
-    int traps = 0;
 
     // Checks if it needs to follow the path or not using visited matriz
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
-            // If I have not visited a square I need a new trap for it
-            if (!visited[i][j]) {
-                dfs(i, j, city, visited);
+            int squareIdentifier = tracker[i][j].second;
+            dfs(i, j, identifier, city, tracker);
+        }
+    }
+
+    int traps = 0;
+    vector<int> graphsId; // Will hold the id of the graphs formed
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            // Checks if the id is already in the graphsId, if not adds it and counts one more trap
+            if (!inside(graphsId, tracker[i][j].second)) {
+                graphsId.push_back(tracker[i][j].second);
                 traps++;
             }
         }
