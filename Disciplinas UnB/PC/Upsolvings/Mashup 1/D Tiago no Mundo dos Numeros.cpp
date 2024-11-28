9802e59e2eb013daf22a0d00aabba235215db57d
Leandro Kornelius <leobelko@gmail.com>
2024-11-28 13:02:00 -0300

    :sparkles: feat: mdc e mmc quest

---
+++ Disciplinas UnB/PC/Upsolvings/Mashup 1/D Tiago no Mundo dos Numeros.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int mdc() {
+
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+ 
+    for (int i = 0; i < t; i++) {
+      int x;
+      cin >> x;
+ 
+      int a = 1, b = x - 1;
+ 
+      cout << a << " " << b << "\n";
+    }
+
+    return 0;
+}
